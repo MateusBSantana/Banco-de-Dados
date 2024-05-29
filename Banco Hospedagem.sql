6ff9eb029e4bad2d38c9a2a6f1aa4d15eb8a7942
Mateus Barboza Santana <67590003+MateusBSantana@users.noreply.github.com>
2024-05-28 21:24:43 -0300

    Banco de dados inicial gerenciamento hospedagem

--- Banco Hospedagem.sql
+++ Banco Hospedagem.sql
@@ -8,7 +8,7 @@ CREATE TABLE hospedes(
 	id_hospede INT AUTO_INCREMENT UNIQUE,
     nome_hospede VARCHAR(40),
     rg VARCHAR(7),
-    cpf VARCHAR(11),
+    cpf VARCHAR(11) UNIQUE,
     data_nascimento DATE,
     sexo CHAR(1),
     celular VARCHAR(13),
@@ -38,26 +38,22 @@ CREATE TABLE funcionarios(
     bairro VARCHAR(40),
     rua VARCHAR(40),
     complemento VARCHAR(40),
-    cargo VARCHAR(20) UNIQUE,
+    cargo VARCHAR(20),
     data_admissao DATE,
     data_emissao_carteira DATE,
     banco VARCHAR(40),
     agencia INTEGER,
     conta INTEGER,
-    status_funcionario VARCHAR(20) UNIQUE,
+    status_funcionario VARCHAR(20),
     observecoes VARCHAR(60),
     PRIMARY KEY (id_funcionario)
 );
 
 CREATE TABLE usuarios(
 	fk_funcionario INT,
-    fk_cargo VARCHAR(20),
-	fk_status_funcionario VARCHAR(20),
     login VARCHAR(40),
     senha VARCHAR(8),
-    FOREIGN KEY (fk_funcionario) REFERENCES funcionarios(id_funcionario),
-    FOREIGN KEY (fk_cargo) REFERENCES funcionarios(cargo),
-	FOREIGN KEY (fk_status_funcionario) REFERENCES funcionarios(status_funcionario)
+    FOREIGN KEY (fk_funcionario) REFERENCES funcionarios(id_funcionario)
 );
     
 
@@ -69,10 +65,13 @@ CREATE TABLE acomodacoes(
     comodidade_wifi BOOLEAN,
     comodidade_arcondicionado BOOLEAN,
     comodidade_tv BOOLEAN,
-    
-    fotos BLOB,
+    comodidade_frigobar BOOLEAN,
+    comodidade_acessibiidade BOOLEAN,
     decricao VARCHAR(40),
-    observecoes VARCHAR(60),
+    bloqueio_acomodacao BOOLEAN,
+    periodo_bloqueio_inicio DATE,
+    periodo_bloqueio_fim DATE,
+    motivo_bloqueio VARCHAR(200),
     PRIMARY KEY (id_acomodacao)
 );
 
@@ -80,10 +79,12 @@ CREATE TABLE reservas(
 	id_reserva INT AUTO_INCREMENT UNIQUE,
     fk_hospede INT,
     fk_acomodacao INT,
-    data_checkin DATE,
+	data_checkin DATE,
     data_checkon DATE,
-    numero_hospedes INTEGER,
-    tipo_acomodacao VARCHAR(20),
+    valor_diaria FLOAT,
+    numero_adulto INTEGER,
+    numero_crianca INTEGER,
+    obeservacoes VARCHAR(200),
     FOREIGN KEY (fk_hospede) REFERENCES hospedes(id_hospede),
     FOREIGN KEY (fk_acomodacao) REFERENCES acomodacoes(id_acomodacao)
 );
